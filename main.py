28c612c9267d219d88ed23180c94bd16de788666
asimonovik <a.simonovik@gmail.com>
2022-04-04 22:13:44 +0200

    update main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+Struga "In ancient times, the Lake Ohrid region, including Enchalon (ancient name of modern Struga) was inhabited by the Illyrian Enchele and Dassareti tribes.[3][4][5][6] The Via Egnatia ran through the Lake Ohrid region, and is believed to have passed west of Enchalon.[7]
+
+Etymologist Qemal Murati believes that the name Strugë-a was first used as the name of a village; this name was used in a document of Tsar Dusan in the 14th century in the form of Struga. Later, in the 16th-17th centuries, the Codex of Slepçan, the name 'Strugi' was used.[8]"
